80bdebd3615bc0e3acb7ae252302f58581586367
hibik17 <75942464+hibik17@users.noreply.github.com>
2021-11-03 23:20:42 +0900

    axios導入

--- frontend/front/src/App.tsx
+++ frontend/front/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from './Button';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "./Button";
 
 function App() {
   return (
