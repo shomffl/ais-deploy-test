fec457d195353711b0ec3348f100791141f020c0
hibik17 <75942464+hibik17@users.noreply.github.com>
2021-11-04 11:47:35 +0900

    useEffeceの追加

--- frontend/front/src/App.tsx
+++ frontend/front/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Button } from "./Button";
 import axios from "axios";
@@ -10,12 +10,20 @@ function App() {
   const [books, setBooks] = useState([]);
 
   // 全ての本の情報を取得
-  axios.get("http://localhost:8000/books").then((response) => {
-    console.log(response);
-    console.log("処理をとおりました");
+  useEffect(() => {
+    axios.get("http://localhost:8000/books").then((response) => {
+      console.log(response);
+      console.log("処理をとおりました");
+    });
   });
 
-  const handleSubmit = () => {
+  const formChange = (e: any) => {
+    setPost(e.target.value);
+    console.log(post);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefalut();
     console.log("handleSubmitが押されました。");
   };
 
@@ -26,9 +34,10 @@ function App() {
           className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           type="text"
           placeholder="What needs to be done?"
+          onChange={formChange}
           // onSubmit={(e: React.KeyboardEvent<HTMLInputElement>) => setPost(e)}
         />
-        <Button>submit</Button>
+        <button type="submit">submit</button>
       </form>
     </div>
   );
