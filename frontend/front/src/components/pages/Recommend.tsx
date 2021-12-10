e4f202549f31462e166224f038726e6a34f7d751
hibik17 <75942464+hibik17@users.noreply.github.com>
2021-12-11 01:50:37 +0900

    card align

--- frontend/front/src/components/pages/Recommend.tsx
+++ frontend/front/src/components/pages/Recommend.tsx
@@ -1,9 +1,10 @@
 import { memo, VFC } from "react";
 import { Header } from "../templates/Header";
+import { NewsCard } from "../templates/NewsCard";
 
 export const Recommend: VFC = memo(() => {
   return (
-    <div className="w-full animate-fade-in-down h-screen bg-gradient-to-br from-gray-700 via-gray-900 to-black">
+    <div className="w-full animate-fade-in-down h-full bg-gradient-to-br from-gray-700 via-gray-900 to-black">
       <Header
         recommendStyle="text-white"
         infoStyle="text-white"
@@ -18,7 +19,18 @@ export const Recommend: VFC = memo(() => {
           Recommended Books
         </p>
       </div>
-      
+      <div className="container mx-auto mt-16 flex flex-wrap">
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+      </div>
     </div>
   );
 });
