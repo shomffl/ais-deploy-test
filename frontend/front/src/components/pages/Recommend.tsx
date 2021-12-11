6894782668303f1cb98f37b2fafb866afb0e6d30
hibik17 <75942464+hibik17@users.noreply.github.com>
2021-12-11 21:12:36 +0900

    backend接続の記述

--- frontend/front/src/components/pages/Recommend.tsx
+++ frontend/front/src/components/pages/Recommend.tsx
@@ -1,9 +1,32 @@
-import { memo, VFC } from "react";
+import axios from "axios";
+import { memo, VFC, useState, useEffect } from "react";
+
 import { RecommendTitle } from "../atom/RecommendTitle";
 import { Header } from "../templates/Header";
 import { NewsCard } from "../templates/NewsCard";
+import { News } from "../types/News";
 
 export const Recommend: VFC = memo(() => {
+  // ニュースの情報を保存しておくstateの宣言
+  const [news, setNews] = useState<News[]>([]);
+
+  // バックエンドからnewsを取得しstateに保存をする処理の記述
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        console.log("ニュースの取得を開始します。");
+        console.log(res.data[0]);
+      })
+      .catch((error) => {
+        alert(error.status);
+      })
+      .finally(() => {
+        console.log(news);
+        console.log("ニュースの取得が終了しました。");
+      });
+  }, news);
+
   return (
     <div className="w-full animate-fade-in-down h-full bg-gradient-to-br from-gray-700 via-gray-900 to-black">
       <Header
