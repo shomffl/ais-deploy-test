658efeb0ba574cc283fe521a5f4757e42717af61
hibik17 <75942464+hibik17@users.noreply.github.com>
2021-12-11 21:16:22 +0900

    modal content revise

--- frontend/front/src/components/templates/BookModal.tsx
+++ frontend/front/src/components/templates/BookModal.tsx
@@ -16,16 +16,19 @@ export const BookModal: VFC<BookModalProps> = memo((props) => {
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                  <h3 className="text-3xl font-semibold">Book title</h3>
+                  <p className="text-gray-300">book auther</p>
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
+                    book summary. book summary. book summary. book summary. book
+                    summary. book summary. book summary. book summary. book
+                    summary. book summary. book summary. book summary. book
+                    summary. book summary. book summary. book summary. book
+                    summary. book summary. book summary. book summary. book
+                    summary. book summary. book summary. book summary. book
+                    summary.
                   </p>
                 </div>
                 {/*footer*/}
