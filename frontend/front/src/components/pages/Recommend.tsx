963f82a1179aae072e7f8ee0b212be67041c44eb
hibik17 <75942464+hibik17@users.noreply.github.com>
2021-12-19 15:52:21 +0900

    axios記述完了

--- frontend/front/src/components/pages/Recommend.tsx
+++ frontend/front/src/components/pages/Recommend.tsx
@@ -4,19 +4,20 @@ import { memo, VFC, useState, useEffect } from "react";
 import { RecommendTitle } from "../atom/RecommendTitle";
 import { Header } from "../templates/Header";
 import { NewsCard } from "../templates/NewsCard";
-import { News } from "../types/News";
+import { Book } from "../types/Book";
 
 export const Recommend: VFC = memo(() => {
   // ニュースの情報を保存しておくstateの宣言
-  const [news, setNews] = useState<News[]>([]);
+  const [news, setNews] = useState<Array<Book>>([]);
 
   // バックエンドからnewsを取得しstateに保存をする処理の記述
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get<Array<Book>>("http://localhost:8000/books")
       .then((res) => {
-        console.log("ニュースの取得を開始します。");
-        console.log(res.data[0]);
+        console.log("ニュースの取得を開始します。↓");
+        console.log(res.data);
+        setNews(res.data);
       })
       .catch((error) => {
         alert(error.status);
@@ -27,6 +28,9 @@ export const Recommend: VFC = memo(() => {
       });
   }, news);
 
+  useEffect(() => {
+    console.log(news);
+  });
   return (
     <div className="w-full animate-fade-in-down h-full bg-gradient-to-br from-gray-700 via-gray-900 to-black">
       <Header
@@ -36,16 +40,10 @@ export const Recommend: VFC = memo(() => {
       />
       <RecommendTitle />
       <div className="container mx-auto mt-16 flex flex-wrap">
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
+        {news.map((todo) => (
+          // この中に繰り返し処理を記述する
+          <></>
+        ))}
       </div>
     </div>
   );
--- frontend/front/src/components/types/Book.tsx
+++ frontend/front/src/components/types/Book.tsx
@@ -1,5 +1,10 @@
 export type Book = {
   id: number;
-  auther: string;
-  summary: string;
+  book_collection_number: number;
+  book_unique_number: number;
+  title: string;
+  author: string;
+  publisher: string;
+  created_at: string;
+  updated_at: string;
 };
