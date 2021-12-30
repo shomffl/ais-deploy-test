829d62bc9618af027ee18d4232ee08ccba84de87
Daiki Hirose <speeed131@gmail.com>
2021-12-30 16:17:02 +0900

    スキーマ例修正

--- api/schemas/news.py
+++ api/schemas/news.py
@@ -3,9 +3,9 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 class News(BaseModel):
-    title: str = Field(example="「日中摩擦」を検証する")
-    summary: str = Field(example="大石 裕")
-    url: str = Field(example="今やメディアの存在と影響を無視しては語れないナショナリズム。2005年春、中国各地で大規模なデモが発生。「愛国無罪」を叫ぶ学生や市民の姿、日本製品不買の呼びかけ、日本の大使館や領事館への投石などが")
+    title: str = Field(example="消される、天安門事件の「記憶」香港の大学、親中派が圧力")
+    summary: str = Field(example="消される天安門事件の記憶。香港の大学、親中派が圧力。香港の大学で、民主化を求める学生らが北京で武力弾圧された1989年の天安門事件")
+    url: str = Field(example="url")
     crawled_at: Optional[str] = Field(example="2021年12月30日11時53分33秒")
 
     class Config:
