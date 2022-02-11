3aa7c9a283cd806615c126ce78b27725401abb23
hibik17 <hibiki@macbook>
2022-02-11 14:15:37 +0900

    loading  追加

--- frontend/front/src/components/pages/Recommend.tsx
+++ frontend/front/src/components/pages/Recommend.tsx
@@ -10,8 +10,12 @@ export const Recommend: VFC = memo(() => {
   // ニュースの情報を保存しておくstateの宣言
   const [news, setNews] = useState<Array<ResponseType>>([]);
 
+  // axiosを叩いてloadingを管理するstatus
+  const [loading, setLoading] = useState(false);
+
   //バックエンドからnewsを取得しstateに保存をする処理の記述;
   useEffect(() => {
+    setLoading(true);
     axios
       .get<Array<ResponseType>>("http://localhost:8000/news-similar-books")
       .then((res) => {
@@ -20,33 +24,45 @@ export const Recommend: VFC = memo(() => {
         setNews(res.data);
       })
       .catch((error) => {
-        alert(error.status);
+        console.log(error.status);
       })
       .finally(() => {
         console.log(news);
         console.log("ニュースの取得が終了しました。");
+        setLoading(false);
       });
   }, news);
 
   useEffect(() => {
     console.log(news);
   });
+
   return (
-    <div className="w-full animate-fade-in-down h-full bg-gradient-to-br from-gray-700 via-gray-900 to-black">
-      <Header
-        recommendStyle="text-white"
-        infoStyle="text-white"
-        isTop={false}
-      />
-      <RecommendTitle />
-      <div className="container mx-auto mt-16 flex flex-wrap">
-        {news.map((res, num) => (
-          // この中に繰り返し処理を記述する
-          <>
-            <NewsCard response={res} key={num} />
-          </>
-        ))}
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <div className="w-full animate-fade-in-down h-full bg-gradient-to-br from-gray-700 via-gray-900 to-black">
+          <Header
+            recommendStyle="text-white"
+            infoStyle="text-white"
+            isTop={false}
+          />
+          <RecommendTitle />
+          <div className="container mx-auto mt-16 flex flex-wrap">
+            {news.map((res, num) => (
+              // この中に繰り返し処理を記述する
+              <>
+                <NewsCard response={res} key={num} />
+              </>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="h-screen bg-gradient-to-br from-gray-700 via-gray-900 to-black">
+          <div className="pt-40 text-center animate-ping text-white text-size-lg">
+            loading...
+          </div>
+        </div>
+      )}
+    </>
   );
 });
